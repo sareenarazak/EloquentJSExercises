793de0d8b8cbb25d911732dd0de925f61aef824f
sareenaabdulrazak <11263177+sareenarazak@users.noreply.github.com>
2024-02-05 19:37:01 -0500

    Ch7 Shortest route robot implementation

--- Ch7.js
+++ Ch7.js
@@ -61,9 +61,9 @@ class VillageState {
 const villageState = VillageState.random();
 
 function runRobot(state, robot, memory) {
-    let turnsTaken  = 0;
+    let turnsTaken;
     for (let turn = 0;; turn++) {
-        if (state.parcels.length === 0) {
+        if (state.parcels.length === 0 || turn === 40) {
             console.log(`Done in ${turn} turns`);
             turnsTaken = turn;
             break;
@@ -147,16 +147,20 @@ function goalOrientedRobot({place, parcels}, route) {
  * For the sake of fairness, make sure you give each task to both robots,
  * rather than generating different tasks per robot.
  */
-function compareRobots(robot1, memory1, robot2, memory2) {
+function compareRobots(robot1, memory1, robot2, memory2, robot3, memory3) {
     let turnsTaken1 = [];
     let turnsTaken2 = [];
+    let turnsTaken3 = []
     for(let round = 0; round < 100; round++) {
         let villageState = VillageState.random();
-        turnsTaken1[round] = runRobot(villageState, routeRobot, memory1);
-        turnsTaken2[round] = runRobot(villageState, goalOrientedRobot, memory2);
+        turnsTaken1[round] = runRobot(villageState, robot1, memory1);
+        turnsTaken2[round] = runRobot(villageState, robot2, memory2);
+        turnsTaken3[round] = runRobot(villageState, robot3, memory3);
+
     }
-    console.log(`Route robot took an average of ${calculateAvg(turnsTaken1)}`);
-    console.log(`Goal oriented robot took an average of ${calculateAvg(turnsTaken2)}`);
+    console.log(`${robot1.name} took an average of ${calculateAvg(turnsTaken1)}`);
+    console.log(`${robot2.name} took an average of ${calculateAvg(turnsTaken2)}`);
+    console.log(`${robot3.name} took an average of ${calculateAvg(turnsTaken3)}`);
 
 }
 
@@ -164,7 +168,31 @@ function calculateAvg(numbers) {
     return numbers.reduce((n1, n2) => n1 + n2)/numbers.length;
 }
 
-compareRobots(routeRobot, [], goalOrientedRobot, []);
+//***** Better way --> 1) instead of robot starting with parcel[0] - we could go through all the parcels and calculate their delivery route
+// sort by the shortest one ( closest) and deliver/pickup them in order
+function shortestRouteRobot({place, parcels}, route) {
+    if(route.length === 0) {
+        let deliverRoutes = [];
+        let pickUpRoutes = [];
+        console.log("Calculating new routes");
 
-//***** Better way --> 1) instead of robot starting with parcel[0] - we could go through all the parcels and find if there is any parcels at this location
-// ******** even better way --> 2 ) construct a shortest path (all to all ) using djikstra and run through the
+        for(let parcel of parcels) {
+            if (parcel.place !== place) {
+                pickUpRoutes.push(findRoute(roadGraph, place, parcel.place));
+            } else {
+               deliverRoutes.push(findRoute(roadGraph, place, parcel.address));
+            }
+        }
+
+        // IMPORTANT : js converts numbers to strings and sorts that way
+        pickUpRoutes.sort((p1, p2) => p1.length - p2.length);
+        deliverRoutes.sort((d1, d2) => d1.length - d2.length);
+
+        // if rout is 0  -->  we need to pick up more parcels
+        route = pickUpRoutes.length === 0 ? deliverRoutes[0] : pickUpRoutes[0];
+    }
+
+    return {direction: route[0] , memory: route.slice(1)};
+}
+
+compareRobots(routeRobot, [], goalOrientedRobot, [], shortestRouteRobot, []);
